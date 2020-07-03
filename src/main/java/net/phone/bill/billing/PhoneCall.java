21e4a8f51faa431fa086318ffe1dd36ab8aabc2f
Moahmmed Thaabit <mohammedthaabitjacobs@gmail.com>
2020-07-03 07:43:35 +0200

    completed phone class and tested

--- src/main/java/net/phone/bill/billing/PhoneCall.java
+++ src/main/java/net/phone/bill/billing/PhoneCall.java
@@ -2,4 +2,18 @@ package net.phone.bill.billing;
 
 public class PhoneCall implements BillAction{
 	
+	private double phoneCallPrice;
+	
+	public PhoneCall(double phoneCallPrice) {
+		
+		if(phoneCallPrice > 0.00) 
+			this.phoneCallPrice = phoneCallPrice;
+		else
+			this.phoneCallPrice = 0.00;
+	}
+	
+	public double totalCost() {
+		return phoneCallPrice;
+	}
+	
 }
