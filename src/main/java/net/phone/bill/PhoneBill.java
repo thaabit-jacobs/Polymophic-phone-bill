917e653d861e29e70c5d94613939836314bb7e18
Mohammed Thaabit Jacobs <mohammedthaabitjacobs@gmail.com>
2020-10-07 14:40:33 +0200

    spark

--- src/main/java/net/phone/bill/PhoneBill.java
+++ src/main/java/net/phone/bill/PhoneBill.java
@@ -2,6 +2,14 @@ package net.phone.bill;
 
 import net.phone.bill.billing.BillAction;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import static spark.Spark.*;
+
 public class PhoneBill {
 	
 	private double totalCost;
@@ -12,5 +20,24 @@ public class PhoneBill {
 	
 	public double total() {
 		return totalCost;
+	}
+
+	public static String render(Map<String, Object> model, String hbsPath)
+	{
+		return new HandlebarsTemplateEngine().render(new ModelAndView(model, hbsPath));
+	}
+
+	public static void main(String[] args)
+	{
+		staticFiles.location("/public");
+
+		get("/", (request, response) -> {
+			Map<String, Object> model = new HashMap<>();
+			//model.put("greet", "Hello world");
+			
+
+
+			return  render(model, "index.hbs");
+		});
 	}
 }
