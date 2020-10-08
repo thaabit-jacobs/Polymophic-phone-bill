ce16e28443d1bb1f3a2e8ab28cb5360a47a0c9d8
Mohammed Thaabit Jacobs <mohammedthaabitjacobs@gmail.com>
2020-10-08 16:08:59 +0200

    completed add methods

--- src/main/java/net/phone/bill/db/BillResources.java
+++ src/main/java/net/phone/bill/db/BillResources.java
@@ -1,5 +1,8 @@
 package net.phone.bill.db;
 
+import net.phone.bill.PhoneBill;
+import net.phone.bill.billing.DataBundle;
+import net.phone.bill.billing.PhoneCall;
 import net.phone.bill.billing.SmsBundle;
 
 import java.sql.Connection;
@@ -17,6 +20,9 @@ public class BillResources {
     private final String addSmsQuery = "INSERT INTO sms(price) VALUES(?)";
     private final String addDataQuery = "INSERT INTO data(price) VALUES(?)";
     private final String addPhoneQuery = "INSERT INTO phone(price) VALUES(?)";
+    private final String clearSmsQuery = "DELETE FROM sms";
+    private final String clearDataQuery = "DELETE FROM data";
+    private final String clearPhoneQuery = "DELETE FROM phone";
 
     public BillResources(Connection connection) {
         try {
@@ -40,7 +46,57 @@ public class BillResources {
             pstmt.close();
         } catch(SQLException se)
         {
-            
+            System.out.println(se + " : Could not add sms price");
+            se.printStackTrace();
+        }
+    }
+
+    public void addData(DataBundle d)
+    {
+        try
+        {
+            pstmt = connection.prepareStatement(addSmsQuery);
+
+            pstmt.setDouble(1, d.totalCost());
+            pstmt.executeUpdate();
+            pstmt.close();
+        } catch(SQLException se)
+        {
+            System.out.println(se + " : Could not add data price");
+            se.printStackTrace();
+        }
+    }
+
+    public void addPhone(PhoneCall p)
+    {
+        try
+        {
+            pstmt = connection.prepareStatement(addPhoneQuery);
+
+            pstmt.setDouble(1, p.totalCost());
+            pstmt.executeUpdate();
+            pstmt.close();
+        } catch(SQLException se)
+        {
+            System.out.println(se + " : Could not add phone price");
+            se.printStackTrace();
+        }
+    }
+
+    public void clearTables()
+    {
+        try
+        {
+            stmt = connection.createStatement();
+            stmt.execute(clearDataQuery);
+            stmt.execute(clearPhoneQuery);
+            stmt.execute(clearSmsQuery);
+            stmt.close();
+
+        }catch(SQLException se)
+        {
+            System.out.println(se + " : Could not clear tables");
+            se.printStackTrace();
         }
     }
 
